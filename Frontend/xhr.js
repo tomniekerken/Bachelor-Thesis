f8efffe921830af5797dbde3bb30435afe709ec8
tomniekerken <tom.niekerken@web.de>
2022-07-03 19:22:03 +0200

    Created xhr.js and added a few tests to it

---
+++ Frontend/xhr.js
@@ -0,0 +1,11 @@
+// characterInteger Form
+const characterIntegerForm = document.getElementById('characterInteger')
+
+characterIntegerForm.addEventListener('submit', event => {
+    event.preventDefault()
+    characterInteger()
+})
+
+function characterInteger() {
+    console.log("hallo")
+}
