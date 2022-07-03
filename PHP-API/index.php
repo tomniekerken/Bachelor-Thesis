9901e6ae0ab46725959ccd3033782a396f65879c
tomniekerken <tom.niekerken@web.de>
2022-07-03 19:22:42 +0200

    Added Methods and a function without usage

--- PHP-API/index.php
+++ PHP-API/index.php
@@ -8,8 +8,27 @@ if ( $_SERVER['REQUEST_METHOD'] === 'OPTIONS' ) {
     $this->responseCode('200');
 }
 
+const METHOD_DELETE = 'DELETE';
+const METHOD_GET = 'GET';
+const METHOD_POST = 'POST';
+const METHOD_PUT = 'PUT';
+
+session_start();
+
 $url_input = filter_input( INPUT_GET, '_url' ) ?? '';
 
-var_dump($url_input);
+switch ($$url_input) {
+    case 'characterInteger':
+        characterInteger();
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+function characterInteger() {
+    
+}
 
 ?>
